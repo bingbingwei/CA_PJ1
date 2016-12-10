b627bdf004e3d8ab558737d4cec7cf8e4eda4dab
bingbingwei <b03902093@ntu.edu.tw>
2016-12-11 04:58:35 +0800

    useless

--- project1_src/Stage4.v
+++ project1_src/Stage4.v
@@ -19,22 +19,16 @@ output [31:0] Data1_o, Data2_o;
 input  [4:0]  RDaddr_i;
 output [4:0]  RDaddr_o;
 
-reg    		  RegWrite, MemtoReg;
-reg    [31:0] Data1, Data2;
-reg	   [4:0]  RDaddr;
-
-assign RegWrite_o_4 = RegWrite;
-assign MemtoReg_o_4 = MemtoReg;
-assign Data1_o = Data1;
-assign Data2_o = Data2;
-assign Rdaddr_o = Rdaddr;
+reg    		  RegWrite_o_4, MemtoReg_o_4;
+reg    [31:0] Data1_o, Data2_o;
+reg	   [4:0]  RDaddr_o;
 
 always @(posedge clk_i) begin
-	RegWrite = RegWrite_i_4;
-	MemtoReg = MemtoReg_i_4;
-	Data1 = Data1_i;
-	Data2 = Data2_i;
-	Rdaddr = Rdaddr_o;
+	RegWrite_o_4 = RegWrite_i_4;
+	MemtoReg_o_4 = MemtoReg_i_4;
+	Data1_o = Data1_i;
+	Data2_o = Data2_i;
+	Rdaddr_o = Rdaddr_o;
 end
 
 endmodule
