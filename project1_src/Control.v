87888e3c0048fcc984049ab8cf57041f2b40ba3b
yu-zhen <wlphg010688@gmail.com>
2016-12-11 01:35:43 +0800

    small modify

--- project1_src/Control.v
+++ project1_src/Control.v
@@ -10,22 +10,22 @@ module Control
 	MemtoReg_o
 );
 
-input			[5:0] Op_i;
-output			ALUSrc_o,RegWrite_o,RegDst_o;
-output			[1:0] ALUOp_o;
+input		[5:0]    Op_i;
+output			   ALUSrc_o,RegWrite_o,RegDst_o;
+output	[1:0]    ALUOp_o;
 
 
-assign			RegDst_o=(Op_i==6'b000000)?   1'b1:1'b0;
-assign			RegWrite_o=(Op_i==6'b000000 
-							|| Op_i==6'b001100 
-							|| Op_i==6'b100011)? 1'b1:1'b0;
-assign			ALUSrc_o=(Op_i==6'b000000)?   1'b0:1'b1;
-assign			MemtoReg_o=(Op_i==6'b100011)? 1'b1:1'b0;
-assign			Memory_write_o=(Op_i==6'b101011)? 1'b1:1'b0;
-assign			Memory_read_o=(Op_i==6'b100011)? 1'b1:1'b0;
-assign			ALUOp_o=(Op_i==6'b000000)? 2'b00://R-type
-						(Op_i==6'b100011 || Op_i==6'b101011)? 2'b01://lw sw :ADD
-						(Op_i==6'b001101)? 2'b10://ori :OR
-						(Op_i==6'b000100)? 2'b11://beq :SUB
-						2'b00;
+assign			RegDst_o=(Op_i==6'b000000)?   1'b1:1'b0;     //R-type-->rd : I-type-->rt
+assign			RegWrite_o=(Op_i==6'b000000                  //R-type
+							|| Op_i==6'b001000                     //addi       
+							|| Op_i==6'b100011)? 1'b1:1'b0;        //lw
+assign			ALUSrc_o=(Op_i==6'b000000)?   1'b0:1'b1;     //R-type : I-type
+assign			MemtoReg_o=(Op_i==6'b100011)? 1'b1:1'b0;     //lw
+assign			Memory_write_o=(Op_i==6'b101011)? 1'b1:1'b0; //sw
+assign			Memory_read_o=(Op_i==6'b100011)? 1'b1:1'b0;  //lw
+assign			ALUOp_o=(Op_i==6'b000000)? 2'b00:            //R-type
+						     (Op_i==6'b100011 || Op_i==6'b101011)? 2'b01://lw sw :ADD
+						     (Op_i==6'b001101)? 2'b10://ori :OR
+						     (Op_i==6'b000100)? 2'b11://beq :SUB
+						      2'b00;
 endmodule
--- project1_src/Control_Sign_Extend.v
+++ project1_src/Control_Sign_Extend.v
@@ -9,11 +9,11 @@ module Control_Sign_Extend(
 	Control_Signal_o
 );
 
-input	RegDst_i,ALUSrc_i,Memory_write_i,Memory_read_i,MemtoReg_i,RegWrite_i,MemtoReg_i;
-input	[1:0] ALUOp_i;
-output	[31:0] Control_Signal_o;
+input	            RegDst_i,ALUSrc_i,Memory_write_i,Memory_read_i,MemtoReg_i,RegWrite_i,MemtoReg_i;
+input	   [1:0]    ALUOp_i;
+output	[31:0]   Control_Signal_o;
 
-assign	Control_Signal_o = {24'b0,RegDst_i
+assign	Control_Signal_o = {24'b0  ,RegDst_i
                                     ,ALUSrc_i
                                     ,ALUOp_i
                                     ,Memory_write_i
